826c70d62eac80f7539849872cd3b529fbdbcc56
Kate <k.burlachenko@treeum.net>
2024-09-13 13:06:16 +0300

    .env settings

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   trailingSlash: process.env._next_intl_trailing_slash === 'true', // Use the environment variable
   env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     _next_intl_trailing_slash: process.env._next_intl_trailing_slash || 'false', // Default to 'false' if not provided
   }
 };
