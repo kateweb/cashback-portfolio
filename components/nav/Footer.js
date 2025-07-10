65a43361039770c38a9fbb09b4a4f5ced2ba9d17
kateburlachenko <k.burlachenko@treeum.net>
2025-07-10 12:02:17 +0300

    SDD-6880 page website user agreement

--- components/nav/Footer.js
+++ components/nav/Footer.js
@@ -8,12 +8,13 @@ const Footer = () => {
   return (
     <footer className="mt-auto py-7 dark-text-white">
       <div className="footer-wrap border-t border-divider border-dashed px-5">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-4 text-sm pt-7">
+        <div className="flex flex-col xl:flex-row justify-between items-center gap-4 text-sm pt-7">
           <div className="text-left dark:text-white">
             © 2012–2025 {t('title')}
           </div>
           <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-4">
-            <a href={"/"+locale+"/user/agreement"} className="text-link hover:underline">{t('agreement')}</a>
+            <a href={"/" + locale + "/user/agreement"} className="text-link hover:underline">{t('agreement')}</a>
+            <a href={"/" + locale + "/user/user_agreement"} className="text-link hover:underline">{t('user_agreement')}</a>
             <a href="#" className="text-link hover:underline">{t('cookie')}</a>
             <a href="#" className="text-link hover:underline">{t('policy')}</a>
           </div>
